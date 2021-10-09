8d62281c510cc6cec12d6baae09e0c7ff3697fc3
Vinicius Marques <v.alvesmarques@gmail.com>
2021-10-09 18:18:59 -0300

    crud model created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,9 @@
+require 'faker'
+
+10.times do
+  Article.create(title: Faker::Lorem.sentence(word_count: 3), content: Faker::Lorem.paragraph(sentence_count: 2))
+end
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
